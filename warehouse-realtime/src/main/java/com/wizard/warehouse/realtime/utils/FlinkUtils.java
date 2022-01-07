b7f303ac776208b014073da78b5c714c52264c4a
sodamnsure <zhaiguoweii5@163.com>
2022-01-07 17:42:36 +0800

    新增flink utils工具类

---
+++ warehouse-realtime/src/main/java/com/wizard/warehouse/realtime/utils/FlinkUtils.java
@@ -0,0 +1,26 @@
+package com.wizard.warehouse.realtime.utils;
+
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * @Author: sodamnsure
+ * @Date: 2022/1/7 11:30 AM
+ * @Desc: flink 工具类
+ */
+public class FlinkUtils {
+    public static void main(String[] args) throws Exception {
+        ParameterTool parameterTool = ParameterTool.fromPropertiesFile(args[0]);
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        long checkpointInterval = parameterTool.getLong("checkpoint.interval", 30000L);
+        String checkpointStorage = parameterTool.getRequired("checkpoint.storage");
+        // enabling and configuring checkpointing
+        env.enableCheckpointing(checkpointInterval);
+        env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
+        env.getCheckpointConfig().setCheckpointStorage(checkpointStorage);
+
+    }
+}
