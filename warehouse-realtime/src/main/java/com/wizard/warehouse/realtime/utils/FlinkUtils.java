ffc878bd22261b086e89d75a995a1ea52c0139cd
sodamnsure <zhaiguoweii5@163.com>
2022-01-10 18:53:23 +0800

    checkpoint  set retain level and consumer whether commit offsets to kafka

--- warehouse-realtime/src/main/java/com/wizard/warehouse/realtime/utils/FlinkUtils.java
+++ warehouse-realtime/src/main/java/com/wizard/warehouse/realtime/utils/FlinkUtils.java
@@ -5,6 +5,7 @@ import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.contrib.streaming.state.EmbeddedRocksDBStateBackend;
 import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 
@@ -33,16 +34,22 @@ public class FlinkUtils {
         env.enableCheckpointing(checkpointInterval);
         // set state backends
         env.setStateBackend(new EmbeddedRocksDBStateBackend(true));
+
+        CheckpointConfig config = env.getCheckpointConfig();
         // set mode to exactly-once
-        env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
-        // sets the checkpoint storage where checkpoint snapshots will be written
-        env.getCheckpointConfig().setCheckpointStorage(checkpointStorage);
+        config.setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
+        // set the checkpoint storage where checkpoint snapshots will be written
+        config.setCheckpointStorage(checkpointStorage);
+        // retain the checkpoint when the job is cancelled.
+        config.enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
 
         // build a kafka source to consume messages
         Properties properties = parameterTool.getProperties();
         List<String> topics = Arrays.asList(parameterTool.getRequired("kafka.input.topics").split(","));
         // subscribing messages from all partitions in a list of topics
         FlinkKafkaConsumer<T> kafkaConsumer = new FlinkKafkaConsumer<>(topics, deserializer.newInstance(), properties);
+        // specifies the consumer should not commit offsets back to Kafka on checkpoints.
+        kafkaConsumer.setCommitOffsetsOnCheckpoints(false);
         return env.addSource(kafkaConsumer);
     }
 
