09b2a3ef62d26aace8da57b737ec89130df4e48b
sodamnsure <zhaiguoweii5@163.com>
2022-01-13 18:06:37 +0800

    creating a data model

---
+++ warehouse-realtime/src/main/java/com/wizard/warehouse/realtime/pojo/DataBean.java
@@ -0,0 +1,70 @@
+package com.wizard.warehouse.realtime.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.flink.table.annotation.DataTypeHint;
+
+import java.util.HashMap;
+
+/**
+ *
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DataBean {
+    private Integer id;
+
+    private String deviceId;
+
+    private String guid;
+
+    private String account;
+
+    private String appId;
+
+    private String appVersion;
+
+    private String carrier;
+
+    private String deviceType;
+
+    private String eventId;
+
+    private String ip;
+
+    private Double latitude;
+
+    private Double longitude;
+
+    private String netType;
+
+    private String osName;
+
+    private String osVersion;
+
+    private String releaseChannel;
+
+    private String resolution;
+
+    private String sessionId;
+
+    private Long timestamp;
+
+    private String newSessionId;
+
+    private String country;
+
+    private String province;
+
+    private String city;
+
+    private String region;
+
+    private HashMap<String, Object> properties;
+
+    private Long lastUpdate;
+
+    private int isNew;
+}
