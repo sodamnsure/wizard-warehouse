7e112b5f10a98c03a9b47d1a16fbfc8df18f5c2f
sodamnsure <zhaiguoweii5@163.com>
2022-01-26 18:22:58 +0800

    use topic、partition、offset as uniq id

--- warehouse-realtime/src/main/java/com/wizard/warehouse/realtime/utils/FlinkUtils.java
+++ warehouse-realtime/src/main/java/com/wizard/warehouse/realtime/utils/FlinkUtils.java
@@ -8,6 +8,7 @@ import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.streaming.connectors.kafka.KafkaDeserializationSchema;
 
 import java.util.Arrays;
 import java.util.List;
@@ -53,4 +54,36 @@ public class FlinkUtils {
         return env.addSource(kafkaConsumer);
     }
 
+    /**
+     * @param parameterTool reading and parsing program arguments from different sources
+     * @param deserializer  defines how to deserialize binaries of Kafka message value or metadata such as topic、partition、offset
+     * @param <T>           type of the returned stream
+     * @return the data stream constructed from kafka
+     */
+    public static <T> DataStream<T> createKafkaStreamWithUniq(ParameterTool parameterTool, Class<? extends KafkaDeserializationSchema<T>> deserializer) throws Exception {
+        // get the String value for the given key.
+        long checkpointInterval = parameterTool.getLong("checkpoint.interval", 30000L);
+        String checkpointStorage = parameterTool.getRequired("checkpoint.storage");
+        // enabling and configuring checkpointing, start a checkpoint every 30000 ms
+        env.enableCheckpointing(checkpointInterval);
+        // set state backends
+        env.setStateBackend(new EmbeddedRocksDBStateBackend(true));
+
+        CheckpointConfig config = env.getCheckpointConfig();
+        // set mode to exactly-once
+        config.setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
+        // set the checkpoint storage where checkpoint snapshots will be written
+        config.setCheckpointStorage(checkpointStorage);
+        // retain the checkpoint when the job is cancelled.
+        config.enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
+
+        // build a kafka source to consume messages
+        Properties properties = parameterTool.getProperties();
+        List<String> topics = Arrays.asList(parameterTool.getRequired("kafka.input.topics").split(","));
+        // subscribing messages from all partitions in a list of topics
+        FlinkKafkaConsumer<T> kafkaConsumer = new FlinkKafkaConsumer<>(topics, deserializer.newInstance(), properties);
+        // specifies the consumer should not commit offsets back to Kafka on checkpoints.
+        kafkaConsumer.setCommitOffsetsOnCheckpoints(false);
+        return env.addSource(kafkaConsumer);
+    }
 }
